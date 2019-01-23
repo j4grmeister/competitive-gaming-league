18df45a7be4f51682894d680a106551a750e7c7b
Nick Greene <nickjg01@gmail.com>
2019-01-23 13:24:08 -0600

    fixed another __init__.py

--- utils/cache.py
+++ utils/cache.py
@@ -1,31 +1,29 @@
-class Cache:
-    def __init__(self):
-        self.data = {}
+data = {}
 
-    def add(self, category, key, value):
-        if category not in self.data:
-            self.data[category] = {}
-        self.data[category][key] = value
+def add(category, key, value):
+    if category not in data:
+        data[category] = {}
+    data[category][key] = value
 
-    def get(self, category, key):
-        if category not in self.data:
-            return None
-        if key not in self.data[category]:
-            return None
-        return self.data[category][key]
+def get(category, key):
+    if category not in data:
+        return None
+    if key not in data[category]:
+        return None
+    return data[category][key]
 
-    def pop(self, categoy, key):
-        if category not in self.data:
-            return None
-        if key not in self.data[category]:
-            return None
-        v = self.data[category][key]
-        del self.data[category][key]
-        return v
+def pop(categoy, key):
+    if category not in data:
+        return None
+    if key not in data[category]:
+        return None
+    v = data[category][key]
+    del sdata[category][key]
+    return v
 
-    def delete(self, category, key):
-        if category not in self.data:
-            return
-        if key not in self.data[category]:
-            return
-        del self.data[category][key]
+def delete(category, key):
+    if category not in data:
+        return
+    if key not in data[category]:
+        return
+    del data[category][key]
