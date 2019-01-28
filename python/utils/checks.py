11740c809f09b11f978300234d0884140a487983
Nick Greene <nickjg01@gmail.com>
2019-01-28 13:49:42 -0600

    pls work

--- python/utils/checks.py
+++ python/utils/checks.py
@@ -12,8 +12,7 @@ def is_registered():
     async def predicate(ctx):
         database.execute(f"SELECT username FROM player_table WHERE discord_id={ctx.author.id};")
         r = (database.fetchone() != None)
-        print(f"name = {ctx.message}")
-        if ctx.command.name != 'help' and not r:
+        if not ctx.message.content.startswith('!help') and not r:
             await ctx.send("You must be registered to use that command.\nYou can register with **!register <username>**")
         return r
     return commands.check(predicate)
@@ -22,7 +21,7 @@ def not_registered():
     async def predicate(ctx):
         database.execute(f"SELECT username FROM player_table WHERE discord_id={ctx.author.id};")
         r = (database.fetchone() == None)
-        if ctx.command.name != 'help' and not r:
+        if not ctx.message.content.startswith('!help') and not r:
             await ctx.send("You have already registered.\nYou can change your name with **!changename <username>**")
         return r
     return commands.check(predicate)
